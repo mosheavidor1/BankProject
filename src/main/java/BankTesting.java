8234c985e13d396077a5c6aa4c91ffaf7593782d
mosheavidor1 <mosheavidorster@gmail.com>
2023-11-26 14:05:58 +0200

    name change

--- src/main/java/BankTesting.java
+++ src/main/java/BankTesting.java
@@ -1,11 +1,16 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 
 public class BankTesting {
@@ -50,5 +55,61 @@ public class BankTesting {
 
         Thread.sleep(4000);
 
+        driver.findElement(By.xpath("//button[normalize-space()='Transactions']")).click();
+
+        Thread.sleep(4000);
+
+
+        WebElement tbodyElement = null;
+
+        try {
+            tbodyElement = driver.findElement(By.xpath("//tbody"));
+        } catch (NoSuchElementException e) {
+            System.out.println("No transactions yet");
+            WebElement backButton = driver.findElement(By.xpath("//button[normalize-space()='Back']"));
+            backButton.click();
+            driver.quit();
+            return; // Exit the method since there are no transactions
+        }
+
+        String tbodyComment = tbodyElement.getAttribute("<tr id=\"anchor0\" ng-repeat=\"tx in transactions | orderBy:sortType:sortReverse | sDate:startDate:end\" class=\"ng-scope\">");
+
+        if (tbodyComment == null || tbodyComment.isEmpty()) {
+            System.out.println("No transactions yet");
+            WebElement backButton = driver.findElement(By.xpath("//button[normalize-space()='Back']"));
+            backButton.click();
+        } else {
+            System.out.println("Transactions found");
+        }
+
+        Thread.sleep(4000);
+        driver.findElement(By.xpath("//button[normalize-space()='Deposit']")).click();
+
+        Thread.sleep(4000);
+
+        driver.findElement(By.xpath("//input[@placeholder='amount']")).sendKeys("200");
+
+        Thread.sleep(4000);
+
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+
+        Thread.sleep(4000);
+        try {
+            // Check if the element with the specified class and text appears
+            WebElement successMessage = driver.findElement(By.xpath("//span[@class='error ng-binding' and text()='Deposit Successful']"));
+
+            if (successMessage.isDisplayed()) {
+                System.out.println("Test succeeded: Deposit Successful");
+            } else {
+                System.out.println("Element found but not displayed");
+            }
+        } catch (NoSuchElementException e) {
+            System.out.println("Element not found: Test failed");
+        }
     }
+
 }
+
+
+
+
